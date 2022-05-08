a325e9d8bdb23a155071a97a2cb4067c74d9081b
ruxming <ruxming@qq.com>
2022-05-09 00:57:43 +0800

    init
    1

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='RuxmingText',
+      version='0.1',
+      description='Process Text in the world',
+      url='https://github.com/ruxming/RuxmingText',
+      author='Ruxming',
+      author_email='ruxming@qq.com',
+      license='MIT',
+      packages=['RuxmingText'],
+      zip_safe=False)
